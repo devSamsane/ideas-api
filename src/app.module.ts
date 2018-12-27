56e910abb1692b2503f05b0a57de4ca07dbcbb02
olivier <lavauxo@gmail.com>
2018-12-27 13:55:52 +0100

    Added GraphQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdeaModule } from './idea/idea.module';
@@ -9,9 +11,15 @@ import { LoggingInterceptor } from './shared/logging.interceptor';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 
-
 @Module({
-  imports: [TypeOrmModule.forRoot(), IdeaModule, UserModule, CommentModule],
+  imports: [TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+    IdeaModule,
+    UserModule,
+    CommentModule,
+  ],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_FILTER,
--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -1,4 +1,6 @@
 import { Injectable, NestInterceptor, ExecutionContext, Logger } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -7,12 +9,23 @@ export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, call$: Observable<any>): Observable<any> {
 
     const req = context.switchToHttp().getRequest();
-    const method = req.method;
-    const url = req.url;
     const now = Date.now();
+    if (req) {
+      const method = req.method;
+      const url = req.url;
 
-    return call$.pipe(
-      tap(() => Logger.log(`${method} ${url} ${Date.now() - now} ms`, context.getClass().name)),
-    );
+      return call$.pipe(
+        tap(() => Logger.log(`${method} ${url} ${Date.now() - now} ms`, context.getClass().name)),
+      );
+    } else {
+      const ctx: any = GqlExecutionContext.create(context);
+      const resolverName = ctx.constructorRef.name;
+      const info = ctx.getInfo();
+
+      return call$.pipe(
+        tap(() => Logger.log(`${info.parentType} "${info.fieldName}" ${Date.now() - now} ms`, resolverName)),
+      );
+    }
+
   }
 }
